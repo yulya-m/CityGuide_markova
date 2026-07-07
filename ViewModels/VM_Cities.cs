0fd0c55e1246278dd2e6049727fa639fb866e210
yulya-m <markovayulya0@yandex.ru>
2026-07-07 23:59:56 +0500

    Разработанная модель представления задач. Маркова

---
+++ ViewModels/VM_Cities.cs
@@ -0,0 +1,37 @@
+﻿using CityGuide29.Markova.Classes;
+using CityGuide29.Markova.Context;
+using CityGuide29.Markova.Models;
+using System.Collections.ObjectModel;
+
+namespace CityGuide29.Markova.ViewModels
+{
+	public class VM_Cities : Notification
+    {
+        public CityContext context = new CityContext();
+        public ObservableCollection<City> Cities { get; set; }
+        public VM_Cities()
+        {
+            Cities = new ObservableCollection<City>(context.Cities.OrderBy(x => x.Name));
+        }
+        public RealyCommand OnAddCity
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    City NewCity = new City()
+                    {
+                        Name = "Новый город",
+                        Country = "Страна",
+                        Description = "Описание",
+                        ImagePath = "/Images/default.jpg"
+                    };
+					Cities.Add(NewCity);
+                    context.Cities.Add(NewCity);
+                    context.SaveChanges();
+				});
+
+            }
+        }
+    }
+}
---
+++ ViewModels/VM_Pages.cs
@@ -0,0 +1,29 @@
+﻿using CityGuide29.Markova.Classes;
+using CityGuide29.Markova.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityGuide29.Markova.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Cities vm_cities = new VM_Cities();
+        public VM_Pages()
+        {
+            MainWindow.init.frame.Navigate(new Main(vm_cities));
+        }
+        public RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}
