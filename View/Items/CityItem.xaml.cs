941a0f66596f73c2e4d7b5c2ef246e88753fb4a3
yulya-m <markovayulya0@yandex.ru>
2026-07-07 22:19:48 +0500

    Разработанная главная страница. Маркова

---
+++ View/Items/CityItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CityGuide29.Markova.View.Items
+{
+	/// <summary>
+	/// Логика взаимодействия для CityItem.xaml
+	/// </summary>
+	public partial class CityItem : UserControl
+	{
+		public CityItem()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ View/Main.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace CityGuide29.Markova.View
+{
+	/// <summary>
+	/// Логика взаимодействия для Main.xaml
+	/// </summary>
+	public partial class Main : Page
+	{
+		public Main(object Context)
+		{
+			InitializeComponent();
+			DataContext = Context;
+		}
+	}
+}
