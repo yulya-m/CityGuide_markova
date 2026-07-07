b26167d1688dd918825992a5f9bd591b6df2fd41
yulya-m <markovayulya0@yandex.ru>
2026-07-07 22:37:37 +0500

    Созданный класс Config. Маркова

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityGuide29.Markova.Classes.Database
+{
+    public class Config
+    {
+		public static readonly string connection = "server=192.168.0.111;" +
+			"uid=root;" +
+			"pwd=root;" +
+			"database=cityguide;";
+		public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 21));
+	}
+}
